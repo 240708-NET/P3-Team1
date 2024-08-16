8583b86f385689cc3d922333b26fdb7558ebea5d
nick wang <nickwang11235@gmail.com>
2024-08-16 14:28:45 -0700

    fixed build warnings

--- UniversityAPI/src/UniversityAPI.Models/Section.cs
+++ UniversityAPI/src/UniversityAPI.Models/Section.cs
@@ -1,40 +1,53 @@
 using System.ComponentModel.DataAnnotations;        //For notation like [Key] and [Required]
-using System.ComponentModel.DataAnnotations.Schema; //For notation like [ForeignKey()]
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection; //For notation like [ForeignKey()]
 
-namespace UniversityAPI.Models
+namespace UniversityAPI.Models;
+
+public class Section
 {
-    public class Section
+    //Attributes
+    [Key]
+    public int ID { get; set; }
+
+    [Required]
+    [ForeignKey("Course")]
+    public int CourseID { get; set; }
+    //Navigation property with lazy loading
+    public virtual Course Course { get; set; }
+
+    [Required]
+    [ForeignKey("Professor")]
+    public int ProfessorID { get; set; }
+    //Navigation property with lazy loading
+    public virtual Professor Professor { get; set; }
+
+    [Required]
+    public TimeSpan StartTime { get; set; }
+
+    [Required]
+    public TimeSpan EndTime { get; set; }
+
+    [Required]
+    [MaxLength(5)]
+    public string Day { get; set; }
+
+    //Constructor
+    public Section()
     {
-        //Attributes
-        [Key]
-        public int ID { get; set; }
+        Course = new Course();
+        Professor = new Professor();
+        Day = "";
+    }
 
-        [Required]
-        [ForeignKey("Course")]
-        public int CourseID { get; set; }
-        //Navigation property with lazy loading
-        public virtual Course Course { get; set; }
-
-        [Required]
-        [ForeignKey("Professor")]
-        public int ProfessorID { get; set; }
-        //Navigation property with lazy loading
-        public virtual Professor Professor { get; set; }
-
-        [Required]
-        public TimeSpan StartTime { get; set; }
-
-        [Required]
-        public TimeSpan EndTime { get; set; }
-
-        [Required]
-        [MaxLength(5)] 
-        public string Day { get; set; }
-
-        //Constructor
-        public Section()
-        {
-
-        }
+    public Section(int courseID, Course course, int professorID, Professor professor, TimeSpan startTime, TimeSpan endTime, string day)
+    {
+        CourseID = courseID;
+        Course = course;
+        ProfessorID = professorID;
+        Professor = professor;
+        StartTime = startTime;
+        EndTime = endTime;
+        Day = day;
     }
 }
