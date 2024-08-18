500eaa7468c256d3ef6342c224c4a7c2fc3d7f02
Daniel <dbellonzi@aol.com>
2024-08-18 14:26:59 -0700

    Added New User Fields

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,17 @@
-import react from "react";
+import react, { useState } from "react";
 import axios, { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 
 const API_BASE = "http://localhost:5236/api";
 
 const LoginPage: react.FC = () => {
-  const [studentID, setStudentID] = react.useState("");
-  const [password, setPassword] = react.useState("");
+  const [ studentID, setStudentID ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ cPassword, setCPassword ] = useState("");
+  const [ fName, setFName ] = useState("");
+  const [ lName, setLName ] = useState("");
+  const [ newUser, setNewUser ] = useState(false);
+  const [ formError, setFormError ] = useState("");
   const nagivate = useNavigate();
 
   const login = (e: react.FormEvent) => {
@@ -30,7 +35,36 @@ const LoginPage: react.FC = () => {
       });
   };
 
-  const registerUser = () => {};
+  const registerUser = () => {
+    if( !newUser ) {
+      setNewUser(true);
+      setFormError("");
+      return;
+    }
+    console.log(fName, lName, password, cPassword);
+    if( password == cPassword ){
+      axios
+        .post(`${API_BASE}/Student/register`, {
+          firstName: fName,
+          lastName: lName,
+          password: password
+        }).then(() => {
+          alert(`Welcome ${fName} ${lName}!`);
+        }).catch((error: AxiosError) => {
+          alert(error.message);
+        })
+    } else {
+      setFormError("Password Fields Do Not Match");
+    }
+  };
+
+  const checkNewUser = () => {
+    if( newUser ) {
+      setNewUser(false);
+      setStudentID("");
+      setFormError("");
+    }
+  }
 
   return (
     <div className="flex flex-col min-h-screen justify-center px-6 py-12 bg-slate-100">
@@ -38,18 +72,52 @@ const LoginPage: react.FC = () => {
         Revature University Course Registration
       </h2>
 
+      { formError && 
+        <div className="mx-auto w-full max-w-sm mt-14">
+          <p className="w-full rounded-md border-0 shadow-sm ring-2 p-2 ring-rose-400 bg-red-200">{formError}</p>
+        </div>
+      }
+
       <div className="mx-auto w-full max-w-sm mt-14">
         <form onSubmit={login} className="space-y-8">
-          <input
-            required
-            type="text"
-            id="studentID"
-            name="studentID"
-            value={studentID}
-            placeholder="Student ID"
-            className="w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 placeholder:text-gray-400"
-            onChange={(e) => setStudentID(e.currentTarget.value)}
-          />
+          { !newUser &&
+            <input
+              required
+              type="text"
+              id="studentID"
+              name="studentID"
+              value={studentID}
+              placeholder="Student ID"
+              className="w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 placeholder:text-gray-400"
+              onChange={(e) => setStudentID(e.currentTarget.value)}
+            />
+          }
+
+          { newUser &&
+            <input
+              required
+              type="text"
+              id="fName"
+              name="fName"
+              value={fName}
+              placeholder="fName"
+              className="w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 placeholder:text-gray-400"
+              onChange={(e) => setFName(e.currentTarget.value)}
+            />
+          }
+
+          { newUser &&
+            <input
+              required
+              type="text"
+              id="lName"
+              name="lName"
+              value={lName}
+              placeholder="lName"
+              className="w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 placeholder:text-gray-400"
+              onChange={(e) => setLName(e.currentTarget.value)}
+            />
+          }
 
           <div className="mx-auto w-full max-w-sm">
             <input
@@ -64,8 +132,23 @@ const LoginPage: react.FC = () => {
             />
           </div>
 
+          { newUser &&
+            <div className="mx-auto w-full max-w-sm">
+              <input
+                type="password"
+                id="cPassword"
+                name="cPassword"
+                value={cPassword}
+                placeholder="Confirm Password"
+                className="w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 placeholder:text-gray-400"
+                onChange={(e) => setCPassword(e.currentTarget.value)}
+              />
+            </div>
+          }
+
           <div>
             <button
+              onClick={checkNewUser}
               type="submit"
               className="w-full rounded-md bg-indigo-600 px-3 py-1.5 leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-2 transition-colors duration-300"
             >
