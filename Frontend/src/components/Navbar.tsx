8a8ce24114cdea8e9eff09a4e7e166da2f395efa
unknown <dbellonzi@aol.com>
2024-08-21 13:49:37 -0700

    added Navigation Bar Component

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
 
-    return(
-        <>
-        </>
+    return (
+        <div className="min-h-16 flex justify-end gap-4 items-center">
+            <Link to={"/login"}>
+                <button
+                    type="button"
+                    value="signup"
+                    className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Login
+                </button>
+            </Link>
+            <Link to={"/search"}>
+                <button
+                    type="button"
+                    value="signup"
+                    className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Search Courses
+                </button>
+            </Link>
+            <Link to={"/mycourses"}>
+                {<button
+                    type="button"
+                    value="signup"
+                    className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    My Courses
+                </button>}
+            </Link>
+        </div>
     )
 }
 
