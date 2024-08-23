66759c9d417b14b777f84ac906fffa5389a29409
Jae Jimmy Wong <jjwong0915@gmail.com>
2024-08-23 17:05:14 -0400

    style overhaul

--- Frontend/src/components/CourseCard.tsx
+++ Frontend/src/components/CourseCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import CourseModal from "./CourseModal";
 import { Course } from "../types";
 
@@ -19,11 +19,13 @@ const Card: React.FC<CardProps> = ({ course, onRegister, onDrop }) => {
     setIsModalOpen(false);
   }
 
-  const handleRegister = () => {
+  const handleRegister: MouseEventHandler = (e) => {
+    e.stopPropagation();
     onRegister(course.id);
   };
 
-  const handleDrop = () => {
+  const handleDrop: MouseEventHandler = (e) => {
+    e.stopPropagation();
     if (course.sectionID) {
       onDrop(course.sectionID);
     }
@@ -31,27 +33,20 @@ const Card: React.FC<CardProps> = ({ course, onRegister, onDrop }) => {
 
   return (
     <>
-      <div className="p-4 border rounded-lg shadow-sm" onClick={handleClick}>
-        <h3 className="text-xl font-semibold text-orange-600">{course.name}</h3>
-        <p className="text-gray-500 text-sm">{course.category}</p>
-
-        <div className="mt-4">
-          {course.isRegistered ? (
-            <button
-              className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-400"
-              onClick={handleDrop}
-            >
-              Drop Course
-            </button>
-          ) : (
-            <button
-              className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-400"
-              onClick={handleRegister}
-            >
-              Register
-            </button>
-          )}
+      <div
+        className="px-4 py-2 flex border justify-between bg-gray-50 hover:bg-white cursor-pointer"
+        onClick={handleClick}
+      >
+        <div className="flex gap-8 items-center">
+          <div className="text-xl">{course.name}</div>
+          <div className="text-sm font-light">{course.category}</div>
         </div>
+        <button
+          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-400"
+          onClick={handleDrop}
+        >
+          Drop
+        </button>
       </div>
       <CourseModal
         course={course}
